62082a2bd50f32bcc350d475a03cc1d26eb4a6ce
irajesicajanuaris <jjiraaa1@gmail.com>
2023-06-22 10:57:44 +0700

    mengecek jika antian hanya ada 1 elemen

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -46,6 +46,11 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+		// cek jika antrian hanya memiliki satu elemen
+		if (FRONT == max - 1) {
+			FRONT = -1;
+			REAR = -1;
+		}
 	}
 
 };
